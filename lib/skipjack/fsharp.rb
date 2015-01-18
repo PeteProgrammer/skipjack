0519b214327c7adbd036a782a3c912ae9ce07046
Peter Strøiman <peter@stroiman.com>
2015-01-18 20:00:36 +0100

    Wrapped task generation code into class
    This allows us to define functions on this class that the custom rake code can
    call when setting the options for the task

--- lib/skipjack/fsharp.rb
+++ lib/skipjack/fsharp.rb
@@ -1,10 +1,23 @@
-def fsc *args, &block
-  Rake::Task::define_task *args do |t|
-    if t.application.windows?
-      cmd = "fsc"
-    else
-      cmd = "fsharpc"
+module Skipjack
+  class FSharpCompiler
+    def initialize *args
+      @args = *args
     end
-    raise "Error executing command" unless Kernel.system "#{cmd} "
+
+    def create_task
+      Rake::Task::define_task *@args do |t|
+        if t.application.windows?
+          cmd = "fsc"
+        else
+          cmd = "fsharpc"
+        end
+        raise "Error executing command" unless Kernel.system "#{cmd} "
+      end
+    end
   end
+end
+
+def fsc *args, &block
+  c = Skipjack::FSharpCompiler.new *args
+  c.create_task
 end
